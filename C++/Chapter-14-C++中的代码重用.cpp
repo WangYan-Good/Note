5dc7f24a126a2931180d74c6ee8151d4ebcbb5d9
WangYan-Good <987639823@qq.com>
2021-04-21 13:06:14 +0800

    Note_C++_Chapter14

--- C++/Chapter-14-C++中的代码重用.cpp
+++ C++/Chapter-14-C++中的代码重用.cpp
@@ -45,3 +45,75 @@ has-a 关系
 
   explicit防止构造函数的隐式转换
 
+14.2 私有继承
+  私有继承是另一种实现has-a关系的途径
+
+  使用包含时将使用对象名来调用方法，而使用私有继承时将使用类名和作用域解析运算符来调用方法，例如：
+  Student类私有继承ArrayDb,调用基类方法
+  double Student::Average() const
+  {
+    if (ArrayDb::size() > 0)
+      return ArrayDb::sum()/ArrayDb::size();
+    else
+      return 0;
+  }
+
+  访问基类对象
+  可以通过强制类型转换完成，将子类对象转换为父类对象，其结果为继承而来的父类对象。
+  例如子类Student获取基类对象的name成员:
+  const string & Student::Name() const
+  {
+    return (const string &) *this;
+  }
+
+  在私有继承中，在不进行显示类型转换的情况下，不能将指向派生类的引用或指针赋给基类引用或者指针
+
+
+  使用包含还是私有继承
+  包含易于理解，针对同类的多个对象便于处理
+
+  继承提供的特性比包含多，针对保护成员，继承是可以使用的，但是组合无法使用
+  当需要重新定义虚函数时，继承可以进行重载，但是组合无法做到。
+
+  保护继承
+  保护继承是私有继承的变体，使用关键字protected
+  class Student:protected std::string,
+                 protected std::valarry<double>
+  { ... };
+
+  使用保护继承时，基类的私有成员无法继承，公有成员和受保护成员都将继承为受保护类型成员。
+
+  使用using重新定义访问权限
+  将函数调用包装在另一个函数调用中，如下所示：
+  class Student : private std::string, private std::valarray<double>
+  {
+  public:
+    using std::valarray<double>::min;
+    using std::valarray<double>::max;
+  };
+
+  以上通过 using 声明，可以令私有继承或保护继承的子类直接调用基类方法。
+  using 声明只使用成员名--没有圆括号、函数特征标和返回类型
+
+  还有一种老式方式可用于在私有派生类中重新声明基类方法，将方法名放在派生类的公有部分，如下：
+  class Student:private std::string,
+                 private std::valarray<double>
+  {
+  public:
+    std::valarray<double>::operator[];
+    ...
+  };
+  该方法目前可能被摒弃
+
+14.3 多重继承
+
+
+
+
+
+
+
+
+
+
+
