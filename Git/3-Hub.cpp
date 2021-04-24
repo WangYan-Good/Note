b1c5ecb9103aca9e5258b34e831b65875a114a23
WangYan-Good <987639823@qq.com>
2021-04-24 22:46:09 +0800

    Note_Git

--- Git/3-Hub.cpp
+++ Git/3-Hub.cpp
@@ -12,18 +12,18 @@
 
   以图示查看提交记录：            git log --graph
 
-  以简洁图示查看提交记录(只显示哈希值和记录)： git log --graph --pretty=format:"%h %s"   
- 
+  以简洁图示查看提交记录(只显示哈希值和记录)： git log --graph --pretty=format:"%h %s"
+
 2.本地忘记提交情况
   地点1：新增功能C1(0%-50%)，但是未提交
   地点2：新增功能C1(50%-100%)，提交成功
 
   地点1：拉取代码(可能会产生冲突)合并
 
-  拉取远程代码： 		git pull origin/dev 
-		        	= 
-  把代码拉到版本库：            git fetch origin/dev 
-                                + 
+  拉取远程代码： 		git pull origin/dev
+		        	=
+  把代码拉到版本库：            git fetch origin/dev
+                                +
   把代码拉到工作区：            git merge origin/dev
 
 3.rebase(变基)应用场景一
@@ -31,21 +31,21 @@
 
   第一种：多个提交记录整合成一个提交记录
          开发进度：C1<-day1<-day2<-day3 ... <-dayn
-  
+
          从当前开始，合并最近的n条记录： git rebase -i HEAD~n
          从当前开始，合并指定版本记录： git rebase -i [版本号]
-  注意事项：合并记录时建议不要合并已push到远程仓库的提交记录       
+  注意事项：合并记录时建议不要合并已push到远程仓库的提交记录
 
 4.rebase应用场景二
   第二种：合并不同分支的提交记录
          master分支：C1 <- C2 <- C4 <- C5
          dev:               <- C3 <-----
-         
-         合并master和dev分支的提交记录：git rebase 
+
+         合并master和dev分支的提交记录：git rebase
   第三种：消除合并代码产生记录分叉
 	 将远程仓库代码拉取到本地工作区： git pull
 	 替换为：
- 	 
+
 	 将远程仓库代码拉取到本地缓冲区： git fetch
 	 将远程仓库与本地合并(包括记录与代码)：git rebase
 
@@ -66,11 +66,11 @@
     git mergetool
 
 7.命令总结
-  添加远程连接：git remote add origin 连接
+  添加远程连接：git remote add origin url
   推送代码：git push url 分支
   下载代码：git clone url
-  拉取代码：git pull url 分支 
-	    等价于 
+  拉取代码：git pull url 分支
+	    等价于
 	    git fetch url 分支
 	    git merge url 分支
   保持代码提交整洁(变基)：git rebase 分支
