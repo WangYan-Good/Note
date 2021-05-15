1fc3adf880c975edf1ef6e11e377dd38bcea7f78
WangYan-Good <987639823@qq.com>
2021-05-15 13:48:13 +0800

    Note_UEFI_20

---
+++ UEFI/20_PCI & PCIe Introduction_Zenger.cpp
@@ -0,0 +1,52 @@
+PCI & PCIe Introduction
+
+1. Agenda
+  PCI & PCIe Overview
+  PCI Introduction
+  PCI Architecture
+  PCI OCnfiguration Space
+  Resource Registers
+  PCI Device List
+  Question
+
+2. PCI & PCIe Voerview
+  PCI - peripheral Component Interconnect
+
+  PCIe - Peripheral Component Interconnect Express
+
+3. PCI Introduction
+  PCI Introduction - What's PCI BUS ?
+  The PCI Local Bus is a high performance 32-bit or 64-bit with multiplexed address and data lines.The bus is intended for use as an interconnect mechanism between highly integrated peripheral controller components, peripheral add-in cards, and processor / memory systems.
+
+4. PCI Configuration Space
+  Device Identification
+    Vendor ID
+    Device ID
+    Revision ID
+    Header Type
+    Class Code :refer PCI Specification 3.0 Appendix D
+    Device Control & Status
+      Device Control
+      Device Status
+    Subsystem Vender ID and Subsystem ID
+    Interrupt Line
+    Interrupt Pin
+    PCI IRQ Router
+    Capabilities Pointer
+    Capabilities List: end 00h
+    Capability ID
+
+5. PCI Bus Enumeration
+
+6. Bus Number Registers
+
+7. Resource Register
+    Memory Base & Memory Limit:
+    I/O Base & I / O Limit
+
+8. PCI Memory Address Map
+
+9. Expansion ROM Base
+
+10. PCI Device List
+  PCI Devices and Functions
