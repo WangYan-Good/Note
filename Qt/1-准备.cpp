be4af4e9c9f5cc3f8b1fb4769ad1c0c3f2d8270d
WangYan-Good <987639823@qq.com>
2021-07-07 01:09:06 +0800

    Note_Qt_CreateProject

---
+++ Qt/1-准备.cpp
@@ -0,0 +1,19 @@
+1. 下载 && 安装
+  download.qt.io
+
+2. 安装说明
+  源码、库、示例、手册、 qtcreator
+
+  Linux 下开发环境准备
+  安装 g++ make
+  sudo apt-get install libgl1-mesa-dev
+
+3. QTCreator调试
+  Windows下调试工具的环境准备 Windows SDK
+  F9 F5 F10 F11 单步断点、变量查看
+
+4. QTCreator项目配置
+  配置库和头文件
+  新增ui和qrc
+  项目构建设置
+  项目运行设置
