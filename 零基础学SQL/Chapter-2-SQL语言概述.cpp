392bd6885863ada9cd53dcfd621407d03858bc58
WangYan-Good <987639823@qq.com>
2021-07-14 01:05:18 +0800

    Note_MySQL_Chapter2

---
+++ 零基础学SQL/Chapter-2-SQL语言概述.cpp
@@ -0,0 +1,18 @@
+第 2 章 SQL语言概述
+  本章重点：
+  SQL语言介绍
+  SQL语句的分类
+  SQL语言的特点
+  SQL语言中常用的数据类型
+  SQL语句书写规范
+
+2.1 SQL 语言介绍
+  数据定义语言( Data Definition Language, DDL )
+  数据查询语言( Data Query Language, DQL )
+  数据操作语言( Data Manipulation Language, DML )
+  数据控制语言( Data Control Language, DCL )
+  事务控制语言
+
+  目前流行的对标准SQL扩展版本主要有两个：PL/SQL 和 Transact-SQL
+  PL/SQL 是基于 Oracle 数据库的通信语言
+  Transact-SQL 是基于Sybase数据库与Microsoft SQL Server数据库中的数据库通信语言
